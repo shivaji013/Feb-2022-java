647059ee801e537395cefdcf615055f854446442
shivaji <shivajixxx13@gmail.com>
2022-02-19 01:11:03 +0530

    Wap to sum of character value

--- SumASCII_value.java
+++ SumASCII_value.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class SumASCII_value {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
 
 		char first;
